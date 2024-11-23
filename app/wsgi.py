3e5ceda2b917ecd719bb414998c19b241294eabf
flystar0526 <flowermetoer@gmail.com>
2024-11-23 16:28:48 +0800

    update flask web

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return f"<h1>Hello World!</h1>"
+  return f"<h1>Hello World!</h1><h3>Flask app is running on {socket.gethostname()}</h3>"
 
 if __name__ == "__main__":
   app.run()
